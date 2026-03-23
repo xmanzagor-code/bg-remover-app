4e2c46dfa52423268a9424fc8f5dedfb8f0b0293
User <user@example.com>
2026-03-23 15:10:09 +0300

    Drastic Android memory fix: 1000px limit and require-corp headers

--- src/utils/bgRemoval.ts
+++ src/utils/bgRemoval.ts
@@ -1,6 +1,6 @@
 import { removeBackground } from '@imgly/background-removal';
 
-const MAX_MOBILE_DIMENSION = 1500;
+const MAX_MOBILE_DIMENSION = 1000;
 
 export const processImage = async (
   imageSource: Blob | string,
